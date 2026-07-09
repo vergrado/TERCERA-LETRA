423479a566505d7937ec05d13994661ba624e007
vergrado <vergrado@gmail.com>
2026-07-09 18:37:41 -0400

    respaldo antes de cambio de carpetas styles

--- src/pages/casos/Casos.jsx
+++ src/pages/casos/Casos.jsx
@@ -26,7 +26,7 @@ import { CaseProvider } from "../../contexts/CaseContext";
 import CasesHeader from "../../components/casos/CasesHeader";
 import CaseFilters from "../../components/casos/CaseFilters";
 import CasesTable from "../../components/casos/CasesTable";
-import "../../styles/cases/cases.css";
+import "../../styles/layout/cases/cases.css";
 // ============================================================
 // COMPONENTE
 // ============================================================
